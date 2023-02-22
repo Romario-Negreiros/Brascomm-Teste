3f475217e8219a453b8d382d0707a6f44e6c78f1
Romario Negreiros <nromario482@gmail.com>
2023-02-22 00:21:40 -0300

    Add hide and show success msg mechanism and format dataCadastro to locale pt-BR string

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,15 +1,23 @@
 const form = document.querySelector('.form')
 const formResetBtn = document.querySelector('.formResetBtn')
+const successMsg = document.querySelector('.success-msg')
 
 const handleFormSubmit = event => {
   event.preventDefault()
   const data = {}
   const { children } = event.target
+
   for (let i = 0; i < children.length - 2; i++) {
     data[children[i].children[0].name] = children[i].children[0].value
   }
-  localStorage.setItem(data.codigo, JSON.stringify(data))
+
+  localStorage.setItem(data.codigo, JSON.stringify({ ...data, dataCadastro: new Date(data.dataCadastro).toLocaleDateString('pt-BR') }))
   formResetBtn.click()
+
+  successMsg.classList.add('show')
+  setTimeout(() => {
+    successMsg.classList.remove('show')
+  }, 5000)
 }
 
 form.addEventListener('submit', handleFormSubmit)
